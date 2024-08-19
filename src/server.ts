90932b7f2c95786acfc826de6f584a89715ff40c
Gabriel Felipe Vieira <ogabriel.v.felipe@gmail.com>
2024-08-19 19:17:24 -0300

    implemented the configuration with database

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,21 @@
 import fastify from 'fastify'
+import { knex } from './database'
+import { env } from './env'
 
 const app = fastify()
 
-app.get('/hello', () => {
-  return 'hello world!'
+app.get('/hello', async () => {
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
+
+  return transaction
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
-  .then(() => {
-    console.log('🚀 HTTP Server Runnig!')
+  .then((e) => {
+    console.log('🚀 HTTP Server Runnig! \n Host: ' + e)
   })
